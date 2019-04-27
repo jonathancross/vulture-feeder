2b91275246c3f56072f9c47fc9299a67bc2f81e9
Jonathan Cross <jcross@gmail.com>
2019-04-27 23:12:28 +0200

    [tests] Adding coloring.

--- test/_run_tests.sh
+++ test/_run_tests.sh
@@ -8,8 +8,13 @@
 # LICENSE: WTFPL
 #          https://github.com/jonathancross/vulture-feeder/blob/master/LICENSE
 ################################################################################
-echo "TESTING vulture-feeder:"
+COLOR_DEFAULT='\033[m\033[40m'
+COLOR_GREEN='\033[32m\033[40m'
+COLOR_GREY='\033[37m\033[40m'
+COLOR_RED='\033[31m\033[40m'
 RESULT_FILE=source_doc.py.new
+
+echo 'TESTING vulture-feeder:'
 
 for TEST in *_test.txt; do
   # Cleanup result file if lingering from before...
@@ -19,14 +24,14 @@ for TEST in *_test.txt; do
   printf "   • %-13s : " "${base#vulture_}"
   cat "${TEST}" | ../vulture-feeder > /dev/null
   if diff "${expected_file}" "${RESULT_FILE}" > /dev/null; then
-    echo "[PASS]"
+    echo "${COLOR_GREEN}[PASS]${COLOR_DEFAULT}"
   else
-    echo "[FAIL] - Test results do not match expected result:"
+    echo "${COLOR_RED}[FAIL]${COLOR_DEFAULT}"' - Test results do not match expected result:'
     diff "${expected_file}" "${RESULT_FILE}"
     # Cleanup results...
     rm -f "${RESULT_FILE}"
-    echo "-------------------------------------------------"
-    echo "You can reproduce this diff by running:"
+    echo '-------------------------------------------------'
+    echo 'You can reproduce this diff by running:'
     echo "cat ${TEST} | ../vulture-feeder > /dev/null; diff ${expected_file} ${RESULT_FILE}"
     break
   fi
