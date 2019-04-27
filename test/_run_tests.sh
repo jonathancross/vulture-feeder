19bce1639125a12a094aaf6ac39f35ef770a13dc
Jonathan Cross <jcross@gmail.com>
2019-04-27 13:29:56 +0200

    [tests] Adding LICENSE, usage, better var naming, etc

--- test/_run_tests.sh
+++ test/_run_tests.sh
@@ -1,21 +1,33 @@
 #!/bin/sh
 # Test vulture-feeder
 #
+# USAGE:   ./_run_tests.sh
+#
+# STATUS:  Currently failing vulture_delete_fifth_expected.txt
+# AUTHOR:  Jonathan Cross 0xC0C076132FFA7695 (jonathancross.com)
+# LICENSE: WTFPL
+#          https://github.com/jonathancross/vulture-feeder/blob/master/LICENSE
+################################################################################
 echo "TESTING vulture-feeder:"
-ACTUAL_RESULT=source_doc.py.new
+RESULT_FILE=source_doc.py.new
 
 for TEST in *_test.txt; do
-  rm -f $ACTUAL_RESULT
-  base=${TEST%_test.txt}
-  expected=${base}_expected.txt
-  printf "   • %-13s : " ${base#vulture_}
-  cat $TEST | ../vulture-feeder > /dev/null
-  if diff "${expected}" $ACTUAL_RESULT > /dev/null; then
+  # Cleanup result file if lingering from before...
+  [ -f "${RESULT_FILE}" ] && rm -f "${RESULT_FILE}"
+  base="${TEST%_test.txt}"
+  expected_file="${base}_expected.txt"
+  printf "   • %-13s : " "${base#vulture_}"
+  cat "${TEST}" | ../vulture-feeder > /dev/null
+  if diff "${expected_file}" "${RESULT_FILE}" > /dev/null; then
     echo "[PASS]"
   else
-    echo "[FAIL] "
-    echo "     ERROR: Test results '$ACTUAL_RESULT' do not match expected result in '${expected}':"
-    diff ${expected} $ACTUAL_RESULT
+    echo "[FAIL] - Test results do not match expected result:"
+    diff "${expected_file}" "${RESULT_FILE}"
+    # Cleanup results...
+    rm -f "${RESULT_FILE}"
+    echo "-------------------------------------------------"
+    echo "You can reproduce this diff by running:"
+    echo "cat ${TEST} | ../vulture-feeder > /dev/null; diff ${expected_file} ${RESULT_FILE}"
     break
   fi
 done
